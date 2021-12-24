e8be98f92587e589ef30b572b73379c86800ded7
WSR <WSR@DESKTOP-731AD6R>
2021-12-24 14:50:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double O, O2, S;
+            //double x = 0;
+            ////Console.WriteLine("Введите x");
+            ////double x = double.Parse(Console.ReadLine());
+            //O = Math.Sqrt(Math.Pow(x, 3/5));
+            //for (int k=1; k<=3;k++)
+            //{
+            //  double S1 = Math.Pow(x, k) / k;
+            //    S = Math.Pow(x, 2) + S1;
+            //}
+            //Console.WriteLine("Ответ: " + S);
+
+            for (double x = -5; x <= 10; x += 0.2)
+            {
+                double y=0;
+                if (x < 12.5) y = Math.Pow(x, 2);
+                else if (x > 12.5) y = Math.Pow(x, 2) - 3 * x + 2.5 * Math.Pow(x, 2);
+                else y = Math.Exp(x) + 5 + Math.Cos(0.001 * x);
+                Console.WriteLine("|{0,13:F1}|{1,14:F2}|",x,y);
+            }
+        }
+    }
+}
